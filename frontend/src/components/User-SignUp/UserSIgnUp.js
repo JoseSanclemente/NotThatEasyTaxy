793eda0a4aeca63053009a6037b81749ecaad54f
JoseSanclemente <jsanclemente@truora.com>
2019-04-05 18:50:39 -0500

    Create a frontend using React

---
+++ frontend/src/components/User-SignUp/UserSIgnUp.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './UserSignUp.css'
+
+class UserSignUp extends Component {
+    render(){
+        return(
+            <div className="SignUp">
+                <div className="SignUp-Container">
+                
+                
+                </div>
+            </div>   
+        )
+    }
+}
+
+export default UserSignUp;
